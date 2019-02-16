7ee054b39350981fbda13a78e56791d9dbb0ad55
hehan502 <961170119@qq.com>
2019-02-16 12:08:25 +0800

    删除了PycharmProjects

--- PycharmProjects/myenv/onetime.py
+++
@@ -1,9 +0,0 @@
-import requests
-from pyquery import PyQuery as pq
-
-html = requests.get('http://www.hbzhan.com/st537293/Article_495568.html')
-html.encoding = 'utf-8'
-doc = pq(html.text)
-
-d = doc('.innewscontent').text()
-print(d)
--- PycharmProjects/myenv/pyfile.py
+++
@@ -1,6 +0,0 @@
-from pyquery import PyQuery as pq
-import requests
-html = requests.get('http://www.163.com').text
-# print(html)
-doc = pq(html)
-print(doc('img'))
